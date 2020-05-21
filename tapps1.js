ed3467070f4d4c688b7db749e2228c0c71ade6c6
thopDB <65649957+thopDB@users.noreply.github.com>
2020-05-21 10:24:36 +0530

    Update tapps1.js
    back to old player

--- tapps1.js
+++ tapps1.js
@@ -290,7 +290,7 @@ function file_video(path){
 	var content = `
 <div class="mdui-container-fluid">
 	<br>
-	<video id="player" class="mdui-video-fluid mdui-center" preload controls>
+	<video class="mdui-video-fluid mdui-center" preload controls>
 	  <source src="${url}" type="video/mp4">
 	</video>
 	<br>${playBtn}
